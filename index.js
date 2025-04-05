177950f497e505a36f2cc218229ce66edbb1ede2
ghulammustafaghori <ghulammustafaghori>
2025-04-06 00:05:43 +0500

    again

--- index.js
+++ index.js
@@ -32,8 +32,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json());
 
 
 app.use('/uploads', express.static('uploads'));
