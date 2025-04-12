13e028e092d3878e7fb1d488fdf7d706864d3adb
ghulammustafaghori <ghulammustafaghori>
2025-04-12 20:01:58 +0500

    new

--- routes/web/featureRestaurantRoutes.js
+++ routes/web/featureRestaurantRoutes.js
@@ -3,7 +3,8 @@ const { featureRestaurantList, insertFeatureRestaurant, featureRestaurantDelete,
 const featureRestaurantRoutes=express.Router();
 
 featureRestaurantRoutes.get('/featureRestaurant-list',featureRestaurantList);
-featureRestaurantRoutes.post('/insert-featureRestaurant/:id',insertFeatureRestaurant);
+featureRestaurantRoutes.post('/feature-existingRestaurant/:id',featureExistingRestaurant);
+featureRestaurantRoutes.post('/insert-featureRestaurant/:id',fileUpload,insertFeatureRestaurant);
 // featureRestaurantRoutes.put('/update-restaurant/:id',restaurantUpdate);
 featureRestaurantRoutes.delete('/delete-featureRestaurant/:id',featureRestaurantDelete);
 
