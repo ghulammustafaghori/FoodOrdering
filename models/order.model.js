170c0b8ec50a6894730b3a0c09bbba1118431c17
ghulammustafaghori <ghulammustafaghori>
2025-04-30 18:15:45 +0500

    new

--- models/order.model.js
+++ models/order.model.js
@@ -1,18 +1,31 @@
-const mongoose=require('mongoose');
-const orderSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-    restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "restaurant" },
-    items: [
-      {
-        menuItemId: { type: mongoose.Schema.Types.ObjectId, ref: "menu" },
-        quantity: Number,
-        price: Number
-      }
-    ],
-    totalPrice: Number,
-    createdAt: { type: Date, default: Date.now },
-  });
-  let orderModel = mongoose.model("Order", orderSchema);
-  module.exports = orderModel;
+const mongoose = require('mongoose');
 
-  
+const orderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  restaurantId: { type: mongoose.Schema.Types.ObjectId, ref: "restaurant" },
+  items: [
+    {
+      menuItemId: { type: mongoose.Schema.Types.ObjectId, ref: "menu" },
+      quantity: Number,
+      price: Number
+    }
+  ],
+  totalPrice: Number,
+
+  // 👇 Add these two fields
+  userAddress: {
+    text: String,
+    latitude: Number,
+    longitude: Number
+  },
+  restaurantAddress: {
+    text: String,
+    latitude: Number,
+    longitude: Number
+  },
+
+  createdAt: { type: Date, default: Date.now },
+});
+
+const orderModel = mongoose.model("Order", orderSchema);
+module.exports = orderModel;
