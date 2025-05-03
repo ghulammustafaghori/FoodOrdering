7d0de36904cb6b5c70054e69a245b01bf32aec8f
ghulammustafaghori <ghulammustafaghori>
2025-05-03 15:00:07 +0500

    new

--- routes/web/orderRoutes.js
+++ routes/web/orderRoutes.js
@@ -1,6 +1,11 @@
 const express= require('express');
 const orderRoutes= express.Router();
 const {orderList,insertOrder}=require('../../controllers/web/orderController');
+
+orderRoutes.post('/test', (req, res) => {
+    console.log("✅ Test route hit!");
+    res.send({ status: 1, message: "Test success" });
+  });
 orderRoutes.get('/order-list',orderList);
 orderRoutes.post('/insert-order',insertOrder);
 module.exports=orderRoutes;
